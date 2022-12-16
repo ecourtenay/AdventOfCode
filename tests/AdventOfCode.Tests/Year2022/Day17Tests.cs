60013e84a66c4525660958c309e082aa188ab139
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-16 22:40:48 +0000

    Fix files

--- tests/AdventOfCode.Tests/Year2022/Day17Tests.cs
+++ tests/AdventOfCode.Tests/Year2022/Day17Tests.cs
@@ -4,19 +4,19 @@ using FluentAssertions;
 
 namespace AdventOfCode.Tests.Year2022;
 
-public class Day04Tests : IClassFixture<Day04>
+public class Day17Tests : IClassFixture<Day17>
 {
-    private readonly Day04 _sut;
+    private readonly Day17 _sut;
 
     private const string TestData = """
     Insert Test Data Here
     """;
 
-    public Day04Tests(Day04 sut)
+    public Day17Tests(Day17 sut)
     {
         _sut = sut;
     }
-    
+
     [Fact(DisplayName = "Part1 should return expected results from example data")]
     public void Part1Example()
     {
