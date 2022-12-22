335339dca7a9d4a7d660270fe900c990b974ce3f
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-22 16:02:24 +0000

    Initial data

--- tests/AdventOfCode.Tests/Year2022/Day22Tests.cs
+++ tests/AdventOfCode.Tests/Year2022/Day22Tests.cs
@@ -4,23 +4,36 @@ using FluentAssertions;
 
 namespace AdventOfCode.Tests.Year2022;
 
-public class Day04Tests : IClassFixture<Day04>
+public class Day22Tests : IClassFixture<Day22>
 {
-    private readonly Day04 _sut;
+    private readonly Day22 _sut;
 
     private const string TestData = """
-    Insert Test Data Here
+            ...#
+            .#..
+            #...
+            ....
+    ...#.......#
+    ........#...
+    ..#....#....
+    ..........#.
+            ...#....
+            .....#..
+            .#......
+            ......#.
+
+    10R5L5R10L4R5L5
     """;
 
-    public Day04Tests(Day04 sut)
+    public Day22Tests(Day22 sut)
     {
         _sut = sut;
     }
-    
+
     [Fact(DisplayName = "Part1 should return expected results from example data")]
     public void Part1Example()
     {
-        _sut.Part1(TestData).As<int>().Should().Be(-1);
+        _sut.Part1(TestData).As<int>().Should().Be(6032);
     }
 
     [Fact(DisplayName = "Part2 should return expected results from example data")]
