bf743c7d56ade90fcce3c36a9c855688d61e67be
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-23 00:13:33 +0000

    Initial data

--- tests/AdventOfCode.Tests/Year2022/Day18Tests.cs
+++ tests/AdventOfCode.Tests/Year2022/Day18Tests.cs
@@ -7,7 +7,19 @@ public class Day18Tests : IClassFixture<Day18>
     private readonly Day18 _sut;
 
     private const string TestData = """
-    Insert Test Data Here
+    2,2,2
+    1,2,2
+    3,2,2
+    2,1,2
+    2,3,2
+    2,2,1
+    2,2,3
+    2,2,4
+    2,2,6
+    1,2,5
+    3,2,5
+    2,1,5
+    2,3,5
     """;
 
     public Day18Tests(Day18 sut)
@@ -18,7 +30,7 @@ public class Day18Tests : IClassFixture<Day18>
     [Fact(DisplayName = "Part1 should return expected results from example data")]
     public void Part1Example()
     {
-        _sut.Part1(TestData).As<int>().Should().Be(-1);
+        _sut.Part1(TestData).As<int>().Should().Be(64);
     }
 
     [Fact(DisplayName = "Part2 should return expected results from example data")]
